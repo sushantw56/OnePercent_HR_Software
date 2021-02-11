a88ae5ff220f0a7e94b30caef6712d833a6da0ee
hemangdtu <hemang.dtu@gmail.com>
2021-02-11 15:33:13 +0530

    Minor change in Employee class

--- lib/employee.dart
+++ lib/employee.dart
@@ -3,5 +3,16 @@ class Employee {
   String profileImg;
   String designation;
 
+  double basicSalary;
+  double totSalary;
+  double gratuityPay;
+  double leaveSalaryPay;
+
+  int tenure;
+
+  int leavesLeft;
+  // Need to create variables to store Timesheet, Overtime
+  // Documents (name, details, expiry), Advance
+
   Employee({this.name, this.profileImg, this.designation});
 }
